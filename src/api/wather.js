fabf957e2d36eb12cbaa73b47a4a4a1e4ced5cd3
Pei Ying <eee317@gmail.com>
2024-09-30 09:39:57 +0800

    加入 .env 環境

--- src/api/wather.js
+++ src/api/wather.js
@@ -7,7 +7,40 @@ const WatherAPI = {
      */
     async getAll() {
         try {
-            const response = await Axios.get('https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWA-CEA2C5AB-A654-4935-9F68-BC88687554CF');
+            const response = await Axios.get(`https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=${import.meta.env.VITE_AUTH_CODE}`);
+            return response.data.records;
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    },
+    async getCity(cityName) {
+        const cityToId = {
+            '嘉義縣': 'F-D0047-029',
+            '新北市': 'F-D0047-069',
+            '嘉義市': 'F-D0047-057',
+            '新竹縣': 'F-D0047-009',
+            '新竹市': 'F-D0047-053',
+            '臺北市': 'F-D0047-061',
+            '臺南市': 'F-D0047-079',
+            '宜蘭縣': 'F-D0047-001',
+            '苗栗縣': 'F-D0047-013',
+            '雲林縣': 'F-D0047-025',
+            '花蓮縣': 'F-D0047-041',
+            '臺中市': 'F-D0047-073',
+            '臺東縣': 'F-D0047-037',
+            '桃園市': 'F-D0047-005',
+            '南投縣': 'F-D0047-021',
+            '高雄市': 'F-D0047-065',
+            '金門縣': 'F-D0047-085',
+            '屏東縣': 'F-D0047-033',
+            '基隆市': 'F-D0047-049',
+            '澎湖縣': 'F-D0047-045',
+            '彰化縣': 'F-D0047-017',
+            '連江縣': 'F-D0047-081',
+        }
+        try {
+            const response = await Axios.get(`https://opendata.cwa.gov.tw/api/v1/rest/datastore/${cityToId[cityName]}${import.meta.env.VITE_AUTH_CODE}`);
             return response.data.records;
         } catch (error) {
             console.error(error);
--- src/components/AllCityWatherDataTabe.vue
+++ src/components/AllCityWatherDataTabe.vue
@@ -2,9 +2,11 @@
 import { onMounted, ref } from "vue";
 import { useToast } from "primevue/usetoast";
 import WatherAPI from "@/api/wather";
+import DataTable from "primevue/datatable";
 
 const toast = useToast();
 const citys = ref([]);
+const city = ref([]);
 const expandedRows = ref({});
 const getAllWather = async () => {
   await WatherAPI.getAll()
@@ -17,32 +19,85 @@ const getAllWather = async () => {
     })
     .finally(() => {});
 };
-const factorTitle = (factorName) => {
-  const factors = {
-    Wx: '天氣現象',
-    PoP: '降雨機率',
-    MinT: '最低溫度',
-    MaxT: '最高溫度',
-    CI: '舒適度'
+const getCityWather = async (cityName) => {
+  await WatherAPI.getCity(cityName)
+    .then((res) => {
+      const cityObj = {
+        locationName: cityName,
+        location: res.locations[0].location
+      }
+      city.value.push(cityObj)
+      console.log('push',city.value)
+      console.log('res', res)
+    })
+}
+const factorTitle = (factorName, type = 1) => {
+  const factorTypes = {
+    1: {
+      Wx: '天氣現象',
+      PoP: '降雨機率',
+      MinT: '最低溫度',
+      MaxT: '最高溫度',
+      CI: '舒適度',
+    },
+    2: {
+      Wx: '',
+      PoP: '%',
+      MinT: '度',
+      MaxT: '度',
+      CI: '',
+    }
   };
-  return factors[factorName] || ''; 
+  
+  return (factorTypes[type]?.[factorName]) || ''; 
 };
+
+const onRowExpand = (event) => {
+  console.log('onRowExpand',event.data.locationName)
+    toast.add({ severity: 'info', summary: 'Product Expanded', detail: event.data.locationName, life: 3000 });
+    const isTrue = city.value.some(item => item.locationName === event.data.locationName); //已經有串接過API的，就不要再觸發
+    if(isTrue === false){
+      getCityWather(event.data.locationName);
+    }
+};
+const onRowCollapse = (event) => {
+  console.log('onRowCollapse',event)
+    toast.add({ severity: 'success', summary: 'Product Collapsed', detail: event.data.locationName, life: 3000 });
+    
+};
+const onRowSelect = (event, isNew = false) => {
+  console.log('event', event)
+  console.log(77788855555)
+}
+// function setExpandedRow($event) {
+//     // check if row expanded before click of not
+//     const isExpanded = expandedRow.value.find((p) => p.id === $event.data.id)
+
+//     if (isExpanded?.id) expandedRow.value = []
+//     else expandedPlans.value = [$event.data]
+// }
+
 onMounted(() => {
   getAllWather();
 });
 </script>
 <template>
-  <div style="max-width:800px; margin-left:auto; margin-right:auto;">
-    <DataTable :value="citys" tableStyle="min-width: 50rem" >
+  
+  <div style="max-width:1000px; margin-left:auto; margin-right:auto;">
+    <Toast></Toast>
+    <DataTable :value="citys" tableStyle="" scrollable scrollHeight="700px"
+      v-model:expandedRows="expandedRows" dataKey="locationName" @rowExpand="onRowExpand" @rowCollapse="onRowCollapse"
+      paginator :rows="50" :rowsPerPageOptions="[5, 10, 20, 50]">
       <template #header>
         <div
-          class="flex flex-wrap align-items-center justify-content-between gap-2"
+          class="flex flex-wrap items-center justify-between gap-2"
         >
-          <span class="text-xl text-900 font-bold">Products</span>
+          <span class="text-xl text-900 font-bold">本日天氣</span>
           <Button icon="pi pi-refresh" rounded raised />
         </div>
       </template>
-      <Column field="locationName" header="Name" ></Column>
+      <Column expander style="width: 5rem" />
+      <Column field="locationName" header="縣市名稱"  ></Column>
       <Column
         v-for="(col, idx) in citys[0]?.weatherElement"
         :key="idx"
@@ -55,13 +110,25 @@ onMounted(() => {
                 ?.time"
               :key="timeIdx"
             >
-              <div v-if="timeIdx===1">
-                {{ time.parameter.parameterName }}
+              <div v-if="timeIdx === 1">
+                {{ time.parameter.parameterName }} {{factorTitle(col.elementName, 2)}}
               </div>
             </div>
           </div>
         </template>
       </Column>
+      <!-- 開合子層 start -->
+      <template  #expansion="slotProps">
+        <div class="p-3">
+          <h5>{{ slotProps.data.locationName }}鄉鎮氣候</h5>
+          
+          <DataTable :value="city[slotProps.data.locationName]">
+            <p>789 {{ city[slotProps.data.locationName] }}</p>
+          </DataTable>
+        </div>
+        
+      </template>
+      <!-- 開合子層 end -->
     </DataTable>
   </div>
   

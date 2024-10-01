77ecca55278a5a6ca78e0740e8562d5d837ff2ed
Pei Ying <eee317@gmail.com>
2024-10-01 09:06:27 +0800

    更改檔案名稱

---
+++ src/components/MyToolbar.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref } from "vue";
+
+
+</script>
+<template>
+  <Toolbar>
+    <template #end>
+      <router-link>首頁</router-link>
+    </template>
+  </Toolbar>
+</template>

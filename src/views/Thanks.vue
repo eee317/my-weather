e5eeb5ba124883b14dc405d6c9f5ed2b4b084289
Pei Ying <eee317@gmail.com>
2024-10-14 09:16:53 +0800

    調整感謝頁的佈局

--- src/views/Thanks.vue
+++ src/views/Thanks.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="text-center">
-    <p>非常感謝您完成問券調查 ! </p>
+  <div class="text-center content-h content-center">
+    <p class="pb-12">非常感謝您完成問券調查 ! </p>
     <router-link :to="{name:'Index'}"><Button label="回首頁" /></router-link>
   </div>
 </template>
+<style scoped lang="scss">
+  .content-h{
+    height: calc(100vh - 176px - 100px);
+  }
+</style>

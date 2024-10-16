f3c0f6e12bf5e0edcd6528ac9196a5369ed4f16d
Pei Ying <eee317@gmail.com>
2024-10-16 18:00:22 +0800

    美化 navbar, footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,18 +1,37 @@
+<script setup>
+import { useRoute } from 'vue-router';
+import { ref } from "vue";
+import { btnNavbarClass } from '../thePreset/customPreset/button/navbarBtn'
+const route = useRoute();
+
+const isActive = (thisRoute) => {
+    return route.name === thisRoute;
+};
+
+</script>
 <template>
-  <div class=" bg-surface-50">
-    
-    <div class="max-w-screen-lg mx-auto py-5 flex justify-center flex-col items-center">
-      <div class="w-100">
-        <div class="mx-3 border-solid border-0 border-b border-primary-100 ">
-          <p class="text-primary-300">資料來源</p>
-          <p class="text-center text-primary-700">交通部中央氣象署 / 氣象開發資料平台</p>
+  <div class="bg-primary-700 py-5">
+    <Toolbar class="mr-auto ml-auto">
+      <template #start>
+        <router-link to="/">
+          <img src="../assets/logo/LOGO-black.svg" alt="">
+        </router-link>
+      </template>
+      <template #end>
+        <div>
+          <div class="text-end">
+            <router-link to="/" class="mr-3">
+              <Button :pt="btnNavbarClass"
+              :class="isActive('Index') ? 'border-primary-700 text-primary-700 bg-white' : 'bg-primary-700 text-white border-white'"
+              label="首頁" outlined /></router-link>
+            <router-link :to="{name:'Questionnaire'}">
+              <Button :pt="btnNavbarClass"
+              :class="isActive('Questionnaire') ? 'border-primary-700 text-primary-700 bg-white' : 'bg-primary-700 text-white border-white'"
+              label="問卷調查" outlined /></router-link>
+          </div>
+          <p class="text-white mt-2 mb-0 text-sm">資料來源 : 交通部中央氣象署 / 氣象開發資料平台</p>
         </div>
-        <div class="text-center pt-1">
-          <router-link to="/"><Button label="首頁" text plain/></router-link>
-          <router-link to="/"><Button label="問卷調查" text plain /></router-link>
-        </div>
-      </div>
-    </div>
+      </template>
+    </Toolbar>
   </div>
-  
 </template>
--- src/components/MyToolbar.vue
+++ src/components/MyToolbar.vue
@@ -1,14 +1,31 @@
 <script setup>
+import { useRoute } from 'vue-router';
 import { ref } from "vue";
+import { btnNavbarClass } from '../thePreset/customPreset/button/navbarBtn'
+const route = useRoute();
 
+const isActive = (thisRoute) => {
+    return route.name === thisRoute;
+};
 
 </script>
 <template>
-  <div class="bg-surface-50 sticky top-0 z-50">
+  <div class="bg-white sticky top-0 z-50 ">
     <Toolbar class="mr-auto ml-auto">
+      <template #start>
+        <router-link to="/">
+          <img src="../assets/logo/LOGO-df.svg" alt="">
+        </router-link>
+      </template>
       <template #end>
-          <router-link to="/"><Button label="首頁" text plain/></router-link>
-          <router-link :to="{name:'Questionnaire'}"><Button label="問卷調查" text plain /></router-link>
+          <router-link to="/" class="mr-4">
+            <Button :pt="btnNavbarClass"
+            :class="isActive('Index') ? 'bg-primary-700 text-white border-none' : 'border-primary-700 text-primary-700 bg-white'"
+            label="首頁" outlined /></router-link>
+          <router-link :to="{name:'Questionnaire'}">
+            <Button :pt="btnNavbarClass"
+            :class="isActive('Questionnaire') ? 'bg-primary-700 text-white border-none' : 'border-primary-700 text-primary-700 bg-white'"
+            label="問卷調查" outlined /></router-link>
       </template>
     </Toolbar>
   </div>

5b28dbc1c245c8db5dd0003e9f2e545811536dbe
Pei Ying <eee317@gmail.com>
2024-10-17 18:02:36 +0800

    首頁切版

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -4,34 +4,27 @@ import { RouterLink } from 'vue-router';
 import WeatherCarousel from '../components/WeatherCarousel.vue';
 import AllEarthquakeTable from '../components/AllEarthquakeTable.vue'
 import Title from '../components/IndexPage/titleComp.vue';
-    
-const today = () => {
-    const today = new Date();
-    const year = today.getFullYear();
-    const month = String(today.getMonth() + 1).padStart(2, '0'); // 月份從 0 開始，所以要加 1，並確保是兩位數
-    const day = String(today.getDate()).padStart(2, '0'); // 確保日期是兩位數
-    const formattedDate = `${year} - ${month} - ${day}`;
-    return formattedDate;
+
+/**
+*將 assets 圖片網址傳遞給元件
+* @param {String} imgName 圖片的檔案名稱
+* @param {String} ext 圖片的副檔名，例如: jpg, png
+* @description 因為元件的關係，單純輸入網址路徑圖片無法顯示，需要透過 new URL 的方式產生圖片
+*/
+
+function  getImageUrl (imgName, ext) { 
+    return new URL(`../assets/img/${imgName}.${ext}`, import.meta.url).href
 }
+
 </script>
 <template>
     <div >
         <div class="weather-bg md:h-[300px] sm:h-[140px] h-[60px] pt-5 ">
-            <!-- <div class="weather-img "></div> -->
             <img class="w-full md:mt-10 h-full" src="../assets/img/cloud.svg" alt="">
         </div>
         <div class="max-w-screen-lg mx-auto  md:mt-[-60px] ">
-            <Title title="本日全臺天氣" :isTime="true" imgSrc="/img/weather-area-img.svg" ></Title>
-            <!-- <Title title="本日全臺天氣" :isTime="true" :imgSrc="require('@/assets/img/weather-area-img.svg')" ></Title> -->
-            <!-- <div class="flex justify-center">
-                <div class="flex items-center gap-5 ">
-                    <img src="../assets/img/weather-area-img.svg" width="110" alt="本日全臺天氣">
-                    <div class="content-center">
-                        <p class="text-5xl  my-0 text-primary-700 font-bold  m-plus-font">本日全臺天氣</p>
-                        <p class=" m-0 pt-2 text-zinc-500 font-bold ">{{ today() }}</p>
-                    </div>
-                </div>
-            </div> -->
+            <Title title="本日全臺天氣" :isTime="true" :imgSrc="getImageUrl('weather-area-img', 'svg')" ></Title>
+            
         </div>
     </div>
     <div class="lg:max-w-screen-lg lg:mx-auto px-4 pb-16 mb-20">
@@ -42,38 +35,54 @@ const today = () => {
                 label="列表查看天氣"  outlined />
         </RouterLink>
         <WeatherCarousel></WeatherCarousel>
-            
-            <!-- <Button label="test" severity="secondary"></Button> -->
     </div>
 
-    <div class="max-w-screen-lg mx-auto mt-20">
-        <Title title="地震資訊" :isTime="false" imgSrc="/img/earthquake-area-img.svg" ></Title>
-
-    </div>
-    <div>
-        <div class="max-w-screen-lg mx-auto px-4 py-16">
-            <p class="text-5xl font-black px-3 mt-20 mb-0">地震資訊</p>
-            <p class="px-3 text-2xl font-black">地震最新報告</p>
-            <p class="px-3">本報告為中央氣象署地震觀測網即時地震資料地震速報之結果</p>
+    <div class="max-w-screen-lg mx-auto my-28">
+        <div class="flex justify-center flex-col items-center relative">
+            <Title class="absolute pb-16" title="地震資訊" :isTime="false" :imgSrc="getImageUrl('earthquake-area-img', 'svg')" ></Title>
+            <img src="../assets/img/grassland.png" class=" w-4/5 mb-[-7.2%] " style="z-index: -10;" alt="">
         </div>
-        <AllEarthquakeTable></AllEarthquakeTable>
+        <div class="relative z-0 mx-6">
+            <div class="border-image p-2 sm:p-10 lg:p-20">
+                <div class="flex items-center gap-3">
+                    <img src="../assets/img/earthquake-symbol.svg" alt="所有地震報告">
+                    <p class="font-semibold m-plus-font text-3xl text-zinc-500 m-0">所有地震報告</p>
+                </div>
+                <p class=" lg:pb-12 text-zinc-500 text-lg">※ 本報告為中央氣象署地震觀測網即時地震資料地震速報之結果</p>
+                <AllEarthquakeTable></AllEarthquakeTable>
+            </div>
+        </div>
     </div>
-    
 </template>
 <style scoped lang="scss">
 .weather-bg{
-    background: linear-gradient(to top, #ffffff  , #BDF4F8 );
-    
+    background: linear-gradient(to top, #ffffff  , #C0F4EA );
 }
 .weather-img{
     background-image: url('../assets/img/cloud.svg');
     background-repeat: repeat-x;
     background-size:contain ;
     height: 100%;
-    
-    // left:-2%;
-    // right:-2%;
-    // top:16%;
-    // z-index: 0;
 }
+.border-image{
+    --borderWidth: 5px;
+    background: #ffff;
+    position: relative;
+    border-radius: var(--borderWidth);
+
+    &:after{
+        content: '';
+        position: absolute;
+        top: calc(-1 * var(--borderWidth));
+        left: calc(-1 * var(--borderWidth));
+        height: calc(100% + var(--borderWidth) * 2);
+        width: calc(100% + var(--borderWidth) * 2);
+        background: linear-gradient(#A2CB8D, #213d10);
+        border-radius: calc(2 * var(--borderWidth));
+        z-index: -1;
+        background-size: 300% 300%;
+    }
+}
+
+
 </style>

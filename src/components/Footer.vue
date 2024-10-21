52d8991b3c899c0d49e9395cf3d171ffd3165a1a
Pei Ying <eee317@gmail.com>
2024-10-21 13:46:40 +0800

    footer, navbar RWD

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,8 @@
 <script setup>
 import { useRoute } from 'vue-router';
 import { ref } from "vue";
-import { btnNavbarClass } from '../thePreset/customPreset/button/navbarBtn'
+import { btnNavbarClass } from '../thePreset/customPreset/button/navbarBtn';
+import toolbarClass from '../thePreset/customPreset/toolbar';
 const route = useRoute();
 
 const isActive = (thisRoute) => {
@@ -11,25 +12,27 @@ const isActive = (thisRoute) => {
 </script>
 <template>
   <div class="bg-primary-800 py-5">
-    <Toolbar class="mr-auto ml-auto">
+    <Toolbar class="mr-auto ml-auto" :pt="{end:{class:'justify-end items-end flex w-full md:w-2/6'}}">
       <template #start>
         <router-link to="/">
-          <img src="../assets/logo/LOGO-black.svg" alt="">
+          <img class="w-[130px] md:w-full" src="../assets/logo/LOGO-black.svg" alt="">
         </router-link>
       </template>
-      <template #end>
-        <div>
+      <template #end class="grow">
+        <div class="content-end">
           <div class="text-end">
             <router-link to="/" class="mr-3">
-              <Button :pt="btnNavbarClass"
+              <Button class="text-xs md:text-base" 
+              :pt="btnNavbarClass"
               :class="isActive('Index') ? 'border-primary-700 text-primary-700 bg-white' : 'bg-primary-800 text-white border-white border-solid'"
               label="首頁" outlined /></router-link>
             <router-link :to="{name:'Questionnaire'}">
-              <Button :pt="btnNavbarClass"
+              <Button  class="text-xs md:text-base"
+              :pt="btnNavbarClass"
               :class="isActive('Questionnaire') ? 'border-primary-700 text-primary-700 bg-white' : 'bg-primary-800 text-white border-white border-solid'"
               label="問卷調查" outlined /></router-link>
           </div>
-          <p class="text-white mt-2 mb-0 text-sm">資料來源 : 交通部中央氣象署 / 氣象開發資料平台</p>
+          <p class="text-white mt-2 mb-0 text-xs  md:text-sm">資料來源 : 交通部中央氣象署 / 氣象開發資料平台</p>
         </div>
       </template>
     </Toolbar>
--- src/components/MyToolbar.vue
+++ src/components/MyToolbar.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { useRoute } from 'vue-router';
 import { ref } from "vue";
-import { btnNavbarClass } from '../thePreset/customPreset/button/navbarBtn'
+import { btnNavbarClass } from '../thePreset/customPreset/button/navbarBtn';
 const route = useRoute();
 
 const isActive = (thisRoute) => {
@@ -11,19 +11,25 @@ const isActive = (thisRoute) => {
 </script>
 <template>
   <div class="bg-white sticky top-0 z-50 ">
-    <Toolbar class="mr-auto ml-auto">
+    <Toolbar :pt="{end:{class:'gap-0'}}" 
+    class="mr-auto ml-auto">
       <template #start>
         <router-link to="/">
-          <img src="../assets/logo/LOGO-df.svg" alt="">
+          <img class="w-[130px] md:w-full" src="../assets/logo/LOGO-df.svg" alt="">
         </router-link>
       </template>
       <template #end>
+
           <router-link to="/" class="mr-4">
-            <Button :pt="btnNavbarClass"
+            <Button 
+            class="text-xs md:text-base"
+            :pt="btnNavbarClass"
             :class="isActive('Index') ? 'bg-primary-700 text-white border-none' : 'border-primary-700 text-primary-700 bg-white border-solid'"
             label="首頁" outlined /></router-link>
           <router-link :to="{name:'Questionnaire'}">
-            <Button :pt="btnNavbarClass"
+            <Button 
+            class="text-xs md:text-base"
+            :pt="btnNavbarClass"
             :class="isActive('Questionnaire') ? 'bg-primary-700 text-white border-none' : 'border-primary-700 text-primary-700 bg-white border-solid'"
             label="問卷調查" outlined /></router-link>
       </template>

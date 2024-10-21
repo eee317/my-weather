5bfe564c6a3dec91df58a3f2edb00ecf6a2ee914
Pei Ying <eee317@gmail.com>
2024-10-21 09:32:11 +0800

    調整標題

--- src/components/EarthquakePage/EarthquakeList.vue
+++ src/components/EarthquakePage/EarthquakeList.vue
@@ -47,19 +47,30 @@ onMounted( async ()=>{
 </script>
 <template>
   <!-- :pt="{column:{ headercell:{class:'bg-primary-100 text-primary-700 dark:bg-primary-dark-500 py-3'}}} -->
+  <div>
+    <p class="title">地震報告</p>
     <DataTable :pt="datatableSide" 
       :value="props.data" tableStyle=""
       v-model:selection="selectedData" selectionMode="single" dataKey="EarthquakeNo">
-    <Column style="width: 3%">
-      <template #body="slotProps">
-        <div class="rounded-full mx-auto w-2 h-2" :class="resultColor(slotProps.data.ReportColor)"></div>
-      </template>
-    </Column>
-    <Column header="地震報告">
-      <template #body="slotProps" >
-        <p class="m-0">{{ matchDate(slotProps.data.ReportContent) }}</p>
-        <p class="m-0">{{ resultTitle(slotProps.data.ReportContent || '').slice(0, 13) }}...</p>
-      </template>
-    </Column>
-  </DataTable>
+      <Column style="width: 3%">
+        <template #body="slotProps">
+          <div class="rounded-full mx-auto w-2 h-2" :class="resultColor(slotProps.data.ReportColor)"></div>
+        </template>
+      </Column>
+      <Column header="地震報告">
+        <template #body="slotProps" >
+          <div class="flex items-center flex-row-reverse justify-end pb-2">
+            <p class="m-0 text-sm">{{ matchDate(slotProps.data.ReportContent) }}</p>
+            <i class="pi pi-calendar mr-2"></i>
+          </div>
+          <p class="m-0">{{ resultTitle(slotProps.data.ReportContent || '').slice(0, 13) }}...</p>
+        </template>
+      </Column>
+    </DataTable>
+  </div>
 </template>
+<style scoped lang="scss">
+  .title{
+    @apply m-0 text-center text-xl font-black text-primary-700 border-b border-t-0 border-x-0 border-primary-500  border-solid pb-2
+  }
+</style>
